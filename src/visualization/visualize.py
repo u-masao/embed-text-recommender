ec7c41a5b235e5c643bce8893b1b737bba503b48
u-masao <4973920+u-masao@users.noreply.github.com>
2023-12-26 22:06:18 +0900

    [update] add search button

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 import time
 
@@ -9,6 +10,9 @@ import pandas as pd
 sys.path.append(".")
 from src.features.build_features import VectorBuilder  # noqa: E402
 
+log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_fmt)
+
 engine = cloudpickle.load(open("data/interim/engine.cloudpickle", "rb"))
 vector_builder = VectorBuilder("oshizo/sbert-jsnli-luke-japanese-base-lite")
 text_df, embeddings = cloudpickle.load(
@@ -19,24 +23,31 @@ text_df["id"] = text_df["id"].astype("int64")
 
 
 def search(query, like_ids):
-    sentences = [x.strip() for x in query.split(" ")]
-    like_ids = [int(x) for x in like_ids.split(" ")]
+    logger = logging.getLogger(__name__)
 
-    like_embeddings = engine.ids_to_embeddings(like_ids)
-    print(like_embeddings.shape)
+    if query.strip():
+        sentences = [x.strip() for x in query.split(" ")]
+        embeddings = vector_builder.encode(sentences)
+        embeddings = np.mean(embeddings, axis=0)
+        logger.info(f"embed.shape: {embeddings.shape}")
+    else:
+        sentences = []
 
-    embeddings = vector_builder.encode(sentences)
-    embeddings = np.mean(embeddings, axis=0)
+    if like_ids.strip():
+        like_ids = [int(x) for x in like_ids.split(" ")]
+        logger.info(f"like_ids: {like_ids}")
+        like_embeddings = engine.ids_to_embeddings(like_ids)
+        logger.info(f"like_embed.shape: {like_embeddings.shape}")
+    else:
+        like_ids = []
+
     start_ts = time.perf_counter()
     similarities, ids = engine.search(embeddings, top_n=30)
     elapsed_time = time.perf_counter() - start_ts
 
     result_df = pd.DataFrame({"id": ids[0], "similarity": similarities[0]})
     result_df = pd.merge(result_df, text_df, on="id", how="left")
-    return (
-        result_df[["id", "similarity", "title", "content", "category"]],
-        elapsed_time,
-    )
+    return result_df[["id", "similarity", "title", "content", "category"]]
 
 
 demo = gr.Interface(fn=search, inputs=["text"], outputs=["text"])
@@ -46,6 +57,7 @@ with gr.Blocks() as demo:
         like_ids = gr.Textbox(
             label="お気に入り記事", show_label=True, value="6588884 6592773"
         )
+        submit_button = gr.Button(value="検索")
         indicator_label = gr.Label(label="indicator")
         output_dataframe = gr.DataFrame(
             label="検索結果", show_label=True, interactive=True
@@ -53,7 +65,12 @@ with gr.Blocks() as demo:
     query_text.submit(
         fn=search,
         inputs=[query_text, like_ids],
-        outputs=[indicator_label, output_dataframe],
+        outputs=[output_dataframe],
+    )
+    submit_button.click(
+        fn=search,
+        inputs=[query_text, like_ids],
+        outputs=[output_dataframe],
     )
 
 
