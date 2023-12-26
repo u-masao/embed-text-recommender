4175b0f04300bf8ae5022d4349ce6762894192d0
u-masao <4973920+u-masao@users.noreply.github.com>
2023-12-26 09:50:39 +0900

    [update] download dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,48 @@
-# -*- coding: utf-8 -*-
-import click
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+
+import click
+import mlflow
+from datasets import load_dataset
+
+
+def make_dataset(kwargs):
+    dataset = load_dataset(
+        kwargs["dataset_name_or_path"],
+        train_ratio=0.8,
+        val_ratio=0.1,
+        test_ratio=0.1,
+        random_state=kwargs["random_state"],
+        suffle=True,
+    )
+
+    print(dataset)
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
+@click.argument("dataset_name_or_path", type=click.Path())
+@click.argument("output_filepath", type=click.Path())
+@click.option("--mlflow_run_name", type=str, default="develop")
+@click.option("--random_state", type=int, default=1234)
+def main(**kwargs):
+    # init logging
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info("start process")
+    mlflow.set_experiments("make_dataset")
+    mlflow.start_run(run_name="mlflow_run_name")
+
+    # log cli options
+    logger.info(f"args: {kwargs}")
+    mlflow.log_params({f"args.{k}": v for k, v in kwargs.items()})
+
+    # process
+    make_dataset(kwargs)
+
+    # cleanup
+    mlflow.end_run()
+    logger.info("complete process")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
