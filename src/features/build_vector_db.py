183f9316133f6623d88c4f44251267396cf25fb3
u-masao <4973920+u-masao@users.noreply.github.com>
2023-12-27 16:26:54 +0900

    [update] vector engine を別ファイルに分割

--- src/features/build_vector_db.py
+++ src/features/build_vector_db.py
@@ -1,66 +1,10 @@
 import logging
-from typing import List
 
 import click
 import cloudpickle
-import faiss
 import mlflow
-import numpy as np
 
-
-class VectorEngine:
-    def __init__(self, ids, embeddings):
-        """
-        VectorEngine を初期化する。
-        ID一覧と埋め込みを利用して内部のDBに登録する。
-
-        Parameters
-        ------
-        ids: List[int]
-            ID
-        embeddings: numpy.ndarray
-            埋め込み表現
-
-        Returns
-        ------
-        None
-        """
-        self.ids = [int(x) for x in list(ids)]
-        self.embeddings = embeddings
-        self.dimension = embeddings.shape[1]
-
-        # normalize
-        l2norms = np.linalg.norm(embeddings, axis=1, ord=2)
-        self.normalized_embeddings = (embeddings.T / l2norms).T
-
-        # init faiss
-        base_index = faiss.IndexFlatIP(self.dimension)
-        self.index = faiss.IndexIDMap(base_index)
-
-        # add vectors to faiss
-        self.index.add_with_ids(self.normalized_embeddings, ids)
-
-    def search(self, query_embeddings, top_n: int = 5):
-        query_embeddings = query_embeddings.reshape(-1, self.dimension)
-        l2norms = np.linalg.norm(query_embeddings, axis=1, ord=2)
-        normalized_query_embeddings = (query_embeddings.T / l2norms).T
-        similarities, indices = self.index.search(
-            normalized_query_embeddings, top_n
-        )
-        return similarities, indices
-
-    def _find_ids_indices(self, ids):
-        results = []
-        for id in ids:
-            if id in self.ids:
-                results.append(self.ids.index(id))
-        return results
-
-    def ids_to_embeddings(self, ids: List[int], normalized: bool = False):
-        if normalized:
-            return self.normalized_embeddings[self._find_ids_indices(ids)]
-        else:
-            return self.embeddings[self._find_ids_indices(ids)]
+from src.models.vector_engine import VectorEngine
 
 
 def build_vector_db(kwargs):
@@ -73,6 +17,8 @@ def build_vector_db(kwargs):
 
     # build vector engine
     engine = VectorEngine(df["id"], embeddings)
+
+    # output vector engine
     cloudpickle.dump(engine, open(kwargs["output_filepath"], "bw"))
 
     # search similar
@@ -85,7 +31,7 @@ def build_vector_db(kwargs):
     logger.info(f"similarities: {similarities}")
     logger.info(f"indices: {indices}")
 
-    # lookup id
+    # lookup ids
     like_ids = [6588884, 6592773]
     like_embeddings = engine.ids_to_embeddings(like_ids)
     logger.info(f"like embeddings: {like_embeddings}")
@@ -95,11 +41,6 @@ def build_vector_db(kwargs):
 @click.command()
 @click.argument("input_filepath", type=click.Path(exists=True))
 @click.argument("output_filepath", type=click.Path())
-@click.option(
-    "--model_name_or_filepath",
-    type=str,
-    default="oshizo/sbert-jsnli-luke-japanese-base-lite",
-)
 @click.option("--mlflow_run_name", type=str, default="develop")
 def main(**kwargs):
     # init logging
