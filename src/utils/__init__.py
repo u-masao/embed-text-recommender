5df99b1335bb2e18dd59325c9f1b9406e35a7aad
u-masao <4973920+u-masao@users.noreply.github.com>
2023-12-29 10:39:08 +0900

    [add] デバイス情報を取得する機能を追加

---
+++ src/utils/__init__.py
@@ -0,0 +1,34 @@
+import torch
+
+
+def get_device_info():
+    """
+    Get device information
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    info : dict
+        Device information
+    """
+    info = {}
+    info["torch"] = torch.__version__
+    info["device.cuda"] = torch.cuda.is_available()
+    info["device.cuda.count"] = torch.cuda.device_count()
+    for i in range(torch.cuda.device_count()):
+        info[f"device.cuda.name.{i}"] = torch.cuda.get_device_name(i)
+        info[f"device.cuda.capability.{i}"] = torch.cuda.get_device_capability(
+            i
+        )
+        info[f"device.cuda.memory.{i}"] = torch.cuda.get_device_properties(
+            i
+        ).total_memory
+        info[f"device.cuda.memory.free.{i}"] = torch.cuda.memory_allocated(i)
+        info[f"device.cuda.memory.max.{i}"] = torch.cuda.max_memory_allocated(
+            i
+        )
+
+    return info
