05f1968c7cf8fd839c8900e546fb57f158c80b00
u-masao <4973920+u-masao@users.noreply.github.com>
2023-12-29 17:45:33 +0900

    [update] issue #17 を再現するコードを追加

--- test/test_embedder.py
+++ test/test_embedder.py
@@ -1,10 +1,30 @@
+import pytest
+
 from src.models import Embedder
 
-def test_return_is_not_none():
-    model_name = "intfloat/multilingual-e5-base"
-    scentences = ["猫と脳波以外のニュース 犬"]
-    # embedder = Embedder(model_name)
-    # embed = embedder.encode(scentences)
-    assert model_name is not None
-    # assert embed is not None
 
+@pytest.fixture(scope="session")
+def embedder():
+    # model_name = "intfloat/multilingual-e5-base"
+    model_name = "intfloat/multilingual-e5-small"
+    _embedder = Embedder(model_name)
+    return _embedder
+
+
+def test_bugfix_check_issue_17(embedder):
+    """
+    https://github.com/u-masao/sbert-news-recommender/issues/17
+    """
+    # 初期化されてるか
+    assert embedder is not None, "Embedder が正しく初期化されてません"
+
+    # 正常ケース
+    scentences = ["猫と脳波以外のニュース", "犬"]
+    embed = embedder.encode(scentences)
+    assert embed is not None
+
+    # 異常ケース
+    # 空文字を入れると重み計算に失敗して Embedder 内の Assert に引っかかる
+    scentences = ["猫と脳波以外のニュース", "", "犬"]
+    embed = embedder.encode(scentences)
+    assert embed is not None
