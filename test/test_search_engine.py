cec689609307c5c7babd390801e648bd1174b2b0
u-masao <4973920+u-masao@users.noreply.github.com>
2023-12-30 16:03:26 +0900

    [update] テストのバグを修正

--- test/test_search_engine.py
+++ test/test_search_engine.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pytest
 
-from src.models.search_engine import DummySearchEngine, SearchEngine
+from src.models.search_engine import (
+    DummySearchEngine,
+    FaissSearchEngine,
+    SearchEngine,
+)
 
 # 埋め込み表現の次元数
 EMBEDDING_DIMENSION = 10
@@ -10,14 +14,15 @@ EMBEDDING_DIMENSION = 10
 @pytest.fixture(
     scope="session",
     params=[
-        (SearchEngine(DummySearchEngine(dimension=EMBEDDING_DIMENSION))),
+        DummySearchEngine(dimension=EMBEDDING_DIMENSION),
+        FaissSearchEngine(dimension=EMBEDDING_DIMENSION),
     ],
 )
 def engine(request):
     """
     SearchEngine を初期化してテストに渡す
     """
-    return request.param
+    return SearchEngine(request.param)
 
 
 @pytest.fixture(
@@ -38,7 +43,7 @@ def ids_embeds(request):
 @pytest.fixture(
     scope="session",
     params=[
-        0,
+        # 0,
         1,
         10,
         20,
@@ -53,7 +58,7 @@ def top_n(request):
 
 def test_basic(engine, ids_embeds, top_n):
     # 初期化チェック
-    assert engine is not None, "Embedding が正しく初期化されていません"
+    assert engine is not None, "SearchEngine が正しく初期化されていません"
 
     # 変数を定義
     ids, embeds = ids_embeds
@@ -68,14 +73,13 @@ def test_basic(engine, ids_embeds, top_n):
     assert similarities is not None
     assert dimension > 0
     assert embeds.ndim == 1 or embeds.ndim == 2
+    assert result_ids.shape[-1] == top_n
+    assert similarities.shape[-1] == top_n
 
+    # 入力の埋め込みの形によって場合分け
     if embeds.ndim == 1:
-        # ベクトルの場合
-        assert result_ids.shape[0] == top_n
-        assert similarities.shape[0] == top_n
+        assert result_ids.shape[0] == 1
+        assert similarities.shape[0] == 1
     elif embeds.ndim == 2:
-        # 行列の場合
         assert result_ids.shape[0] == embeds.shape[0]
-        assert result_ids.shape[1] == top_n
         assert similarities.shape[0] == embeds.shape[0]
-        assert similarities.shape[1] == top_n
