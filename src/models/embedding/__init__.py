55012def04bcdb1fdf8e81a138e43c8e4fece154
u-masao <4973920+u-masao@users.noreply.github.com>
2023-12-30 13:29:55 +0900

    [update] SentenceTransfomerEmbedding を追加

--- src/models/embedding/__init__.py
+++ src/models/embedding/__init__.py
@@ -1,8 +1,10 @@
 from .dummy_embedding import DummyEmbedding
 from .embedding_model import EmbeddingModel, EmbeddingStrategy
+from .sentence_transfomer_embedding import SentenceTransformerEmbedding
 
 __all__ = [
     "EmbeddingModel",
     "EmbeddingStrategy",
     "DummyEmbedding",
+    "SentenceTransformerEmbedding",
 ]
