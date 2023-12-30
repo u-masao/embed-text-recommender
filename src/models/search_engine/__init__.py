4a03413e17da37badba86e6860b29a431ba8d663
u-masao <4973920+u-masao@users.noreply.github.com>
2023-12-30 12:33:06 +0900

    [update] SearchEngine, DummySearchEngine を実装

---
+++ src/models/search_engine/__init__.py
@@ -0,0 +1,4 @@
+from .dummy_search_engine import DummySearchEngine
+from .search_engine import SearchEngine, SearchEngineStrategy
+
+__all__ = ["SearchEngineStrategy", "SearchEngine", "DummySearchEngine"]
