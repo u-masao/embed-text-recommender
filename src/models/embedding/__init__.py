2c8b1f26a9af1102c92bef66f1131db93932a2e7
u-masao <4973920+u-masao@users.noreply.github.com>
2023-12-30 12:31:57 +0900

    [update] code formatting

--- src/models/embedding/__init__.py
+++ src/models/embedding/__init__.py
@@ -1,5 +1,5 @@
-from .embedding_model import EmbeddingModel, EmbeddingStrategy
 from .dummy_embedding import DummyEmbedding
+from .embedding_model import EmbeddingModel, EmbeddingStrategy
 
 __all__ = [
     "EmbeddingModel",
