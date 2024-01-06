f3cbacf4b80b9d238d1efd9b223c63d036e8cb1f
u-masao <4973920+u-masao@users.noreply.github.com>
2024-01-06 12:15:19 +0900

    [update] parse log

--- src/visualization/query_result.py
+++ src/visualization/query_result.py
@@ -1,8 +1,18 @@
 # import streamlit as st
 import glob
 import json
+import re
 
 import pandas as pd
+
+
+def filter_model_name(model_string):
+    result = ""
+    for line in model_string.split("\n"):
+        if "model_name_or_filepath" in line:
+            result = re.sub("model_name_or_filepath': '", "", line)
+    result = result.replace("'", "").replace(",", "").strip()
+    return result
 
 
 def main():
@@ -16,7 +26,7 @@ def main():
     raw_df = pd.DataFrame(runs)
     df = pd.DataFrame()
     df["model_name"] = raw_df["configuration"].map(
-        lambda x: x["embedding_model"]["batch_size"]
+        lambda x: filter_model_name(x["embedding_model"])
     )
     df["id"] = pd.DataFrame(raw_df["outputs"]).map(lambda x: x["result"]["id"])
     df["query"] = pd.DataFrame(raw_df["inputs"]).map(
@@ -24,6 +34,7 @@ def main():
     )
 
     print(raw_df.columns)
+    print(df.iloc[0]["model_name"])
     print(df)
 
 
