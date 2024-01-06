b9f34c82fa4d439ad08b1e0279515e3c1b3a3ce3
u-masao <4973920+u-masao@users.noreply.github.com>
2024-01-06 13:09:32 +0900

    [update] ログ結合を実装

--- src/visualization/query_result.py
+++ src/visualization/query_result.py
@@ -1,18 +1,8 @@
 # import streamlit as st
 import glob
 import json
-import re
 
 import pandas as pd
-
-
-def filter_model_name(model_string):
-    result = ""
-    for line in model_string.split("\n"):
-        if "model_name_or_filepath" in line:
-            result = re.sub("model_name_or_filepath': '", "", line)
-    result = result.replace("'", "").replace(",", "").strip()
-    return result
 
 
 def main():
@@ -22,20 +12,36 @@ def main():
     ):
         run = json.load(open(filepath, "r"))
         runs.append(run)
+        print(filepath)
 
     raw_df = pd.DataFrame(runs)
     df = pd.DataFrame()
-    df["model_name"] = raw_df["configuration"].map(
+    df["embedding_model"] = raw_df["configuration"].map(
         lambda x: json.loads(x["embedding_model"])
+    )
+    df["engine"] = raw_df["configuration"].map(
+        lambda x: json.loads(x["engine"])
     )
     df["id"] = pd.DataFrame(raw_df["outputs"]).map(lambda x: x["result"]["id"])
     df["query"] = pd.DataFrame(raw_df["inputs"]).map(
         lambda x: x["positive_query"]
     )
 
-    print(raw_df.columns)
-    print(df.iloc[0]["model_name"])
-    print(df)
+    print(df.columns)
+    rankings = []
+    for index, item in df.iterrows():
+        ranking_df = pd.DataFrame(
+            [v for k, v in item["id"].items()], columns=["ranking"]
+        )
+        ranking_df["model_name"] = item["embedding_model"][
+            "model_name_or_filepath"
+        ]
+        ranking_df["query"] = item["query"]
+        rankings.append(ranking_df)
+
+    ranking_df = pd.concat(rankings)
+    ranking_df.to_csv("data/processed/ranking.csv", encoding="cp932")
+    print(ranking_df)
 
 
 if __name__ == "__main__":
