352b356992e978679f5da2352b19635a7ce31f7d
u-masao <4973920+u-masao@users.noreply.github.com>
2024-01-06 10:28:40 +0900

    [update] code formatting

--- src/visualization/query_result.py
+++ src/visualization/query_result.py
@@ -1,23 +1,31 @@
 # import streamlit as st
-import pandas as pd
+import glob
 import json
 
-import glob
+import pandas as pd
+
+
 def main():
     runs = []
-    for filepath in glob.glob('data/processed/scenario/**/*.json', recursive=True):
-        run = json.load(open(filepath,'r'))
+    for filepath in glob.glob(
+        "data/processed/scenario/**/*.json", recursive=True
+    ):
+        run = json.load(open(filepath, "r"))
         runs.append(run)
 
     raw_df = pd.DataFrame(runs)
     df = pd.DataFrame()
-    df['model_name'] = raw_df['configuration'].map(lambda x: x['embedding_model']['batch_size'])
-    df['id'] = pd.DataFrame(raw_df['outputs']).map(lambda x: x['result']['id'])
-    df['query'] = pd.DataFrame(raw_df['inputs']).map(lambda x: x['positive_query'])
+    df["model_name"] = raw_df["configuration"].map(
+        lambda x: x["embedding_model"]["batch_size"]
+    )
+    df["id"] = pd.DataFrame(raw_df["outputs"]).map(lambda x: x["result"]["id"])
+    df["query"] = pd.DataFrame(raw_df["inputs"]).map(
+        lambda x: x["positive_query"]
+    )
 
     print(raw_df.columns)
     print(df)
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
